0e4ffe31feb5d55cdc3f875f1cbaf88eca5f8c68
Hiago De Franco <hiagofranco@gmail.com>
2025-09-03 23:14:44 -0300

    implement move_cursor with io file

--- kmain.c
+++ kmain.c
@@ -1,3 +1,12 @@
+#include "io.h"
+
+/* The I/O ports */
+#define FB_COMMAND_PORT		0x3D4
+#define FB_DATA_PORT		0x3D5
+
+/* The I/O port commands */
+#define FB_HIGH_BYTE_COMMAND	14
+#define FB_LOW_BYTE_COMMAND	15
 
 #define FB_START		0x000b8000
 #define FB_BLACK		0
@@ -18,6 +27,19 @@
 #define FB_WHITE		15
 
 char *fb = (char *) FB_START; // for now, global defined frame buffer
+
+/** fb_move_cursor:
+ * Moves the cursor of the framebuffer to the give position
+ *
+ * @param pos The new position of the cursor
+ */
+void fb_move_cursor(unsigned short pos)
+{
+	outb(FB_COMMAND_PORT, FB_HIGH_BYTE_COMMAND);
+	outb(FB_DATA_PORT, ((pos >> 8) & 0x00ff));
+	outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
+	outb(FB_DATA_PORT, pos & 0x00ff);
+}
 
 /** fb_write_cell:
  * Writes a character with the given foreground and background to position i in
@@ -40,5 +62,8 @@ void kmain(void)
 
 	for (i = 0; i < sizeof(hello_world); i++)
 		fb_write_cell(i * 2, hello_world[i], FB_WHITE, FB_BLACK);
+
+	fb_move_cursor(0x50);
+
 	for(;;);
 }
