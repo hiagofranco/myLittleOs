860a46710b94089940370fd970f24fb0083569db
Hiago De Franco <hiagofranco@gmail.com>
2025-09-10 21:23:51 -0300

    add inital serial port driver

---
+++ drivers/serial/ser.h
@@ -0,0 +1,7 @@
+#ifndef INCLUDE_SER_H
+#define INCLUDE_SER_H
+
+void serial_configure_baud_rate(unsigned short com, unsigned short divisor);
+void serial_configure_line(unsigned short com);
+
+#endif /* INCLUDE_SER_H */
